c237c4c4209f3f6f948a52d975f23f2a919daa62
linpz <103488283+linpz@users.noreply.github.com>
2023-09-01 23:35:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,77 +1,52 @@
+pip install line-bot-sdk
 from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from qa_model import qa_function  # 导入你的 QA 模型函数的方式可能不同，根据实际情况修改导入语句
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import openai
-import time
-#======python的函數庫==========
 
 app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-# OPENAI API Key初始化設定
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
+# 用你的 Channel Secret 和 Channel Access Token 初始化 LineBotApi 和 WebhookHandler
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
 
-def GPT_response(text):
-    # 接收回應
-    response = openai.Completion.create(model="text-davinci-003", prompt=text, temperature=0.5, max_tokens=500)
-    print(response)
-    # 重組回應
-    answer = response['choices'][0]['text'].replace('。','')
-    return answer
-
-
-# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
+    # 获取请求的头部信息
     signature = request.headers['X-Line-Signature']
-    # get request body as text
+
+    # 获取请求的主体内容
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
+
     try:
+        # 验证请求的签名是否正确，不正确则抛出异常
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-    return 'OK'
 
+    return 'OK'
 
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    GPT_answer = GPT_response(msg)
-    print(GPT_answer)
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
+    GPT_answer = GPT_response(msg)  # 使用 GPT 模型获取初始回答
 
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
+    # 使用 QA 模型获取更准确的回答
+    qa_answer = qa_function(msg)
+
+    # 输出 QA 模型的回答
+    print(qa_answer)
+
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(qa_answer))
 
 
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
-        
+
+
+if __name__ == "__main__":
+    app.run()
         
 import os
 if __name__ == "__main__":
