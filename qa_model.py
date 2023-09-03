bbfdaf14056106e2449b1f3ced1576cd78d8f775
linpz <103488283+linpz@users.noreply.github.com>
2023-09-03 21:07:38 +0800

    Update qa_model.py

--- qa_model.py
+++ qa_model.py
@@ -1,22 +1,12 @@
 import csv
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
-from linebot import WebhookHandler, LineBotApi
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
-import os
-
-#======這裡是呼叫的檔案內容=====
-from qa_function import *
-#======這裡是呼叫的檔案內容=====
-
-
+from linebot.models import TextSendMessage
 # 設定 OpenAI API 金鑰
-os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')  # 請更換為您的 API 金鑰
-
-# 初始化 LINE Bot
-line_bot_api = LineBotApi("CHANNEL_ACCESS_TOKEN")
-handler = WebhookHandler("CHANNEL_SECRET")
+import os
+os.environ["OPENAI_API_KEY"] ="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"  # 請更換為您的 API 金鑰
 
 class Document:
     def __init__(self, content, metadata=None):
@@ -32,10 +22,10 @@ class CSVLoader:
             reader = csv.reader(file)
             return [Document(", ".join(row)) for row in reader]
 
-def initialize_qa_system(file_path):
+def send_message(query=None):
+
     # 指定檔案路徑
-    # file_path = "test.csv"  # 請更換為您的 CSV 文件路徑
-    file_path = os.path.join(os.getcwd(), "test.csv")  # 使用当前工作目录
+    file_path = "/content/test.csv"  # 請更換為您的 CSV 文件路徑
 
     # 建立加載器並加載文本
     loader = CSVLoader(file_path)
@@ -48,10 +38,13 @@ def initialize_qa_system(file_path):
     # 建立對話鏈
     qa = ConversationalRetrievalChain.from_llm(ChatOpenAI(temperature=0.2), vectorstore.as_retriever())
 
-    return qa
-
-if __name__ == "__main__":
-    qa_system = initialize_qa_system("test.csv")
-    chat_history = []  # 初始化聊天历史
+    chat_history = []
+    while True:
+        if query == None:
+            break
+        result = qa({"question": query + '(用繁體中文回答)', "chat_history": chat_history})
+        print('A:', result['answer'])
+        chat_history.append((query, result['answer']))
+    return TextSendMessage(result)
    
 
